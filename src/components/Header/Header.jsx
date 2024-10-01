764bb4487977377e8f575a15092b8d6828a716f9
Azreil-OFD <fantom2413@gmail.com>
2024-10-01 13:23:17 +0500

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-
-import './Header.css'
+import { useLocation, useNavigate } from "react-router-dom";
+import './Header.css';
 import { useTelegram } from "../../hooks/useTelegram";
+
 const Header = (props) => {
-    const {user} = useTelegram()
-      
+    const { user } = useTelegram();
+    const location = useLocation();  // Хук для получения текущего маршрута
+    const navigate = useNavigate();  // Хук для навигации
+
+    const isIndexPage = location.pathname === "/";
+
+    const handleGoBack = () => {
+        navigate(-1);  // Навигация назад
+    };
+
     return (
-       <div className={'header'}>
-            <span className={'username'}>Добро пожаловать{user?.username ? ', ' : ''}{user?.username}!</span>
-       </div>
-    )
-}
+        <div className={'header'}>
+            {!isIndexPage && (
+                <button className="back-button" onClick={handleGoBack}>
+                    Назад
+                </button>
+            )}
+            <span className={'username'}>
+                Добро пожаловать{user?.username ? ', ' : ''}{user?.username}!
+            </span>
+        </div>
+    );
+};
 
 export default Header;
