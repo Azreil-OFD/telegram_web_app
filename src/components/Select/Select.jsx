648f450c56ad8c452928545a5edd102b5ac217f7
Azreil-OFD <fantom2413@gmail.com>
2024-10-01 15:05:24 +0500

    fix

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,27 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import './Select.css'; // Файл с кастомными стилями
 
 const Select = ({ start, end, onSelect }) => {
+    const [selection, setSelection] = useState(-1);
 
-  const handleClick = (value) => {
-    onSelect(value); // Вызываем переданную функцию
-  };
+    const handleClick = (value) => {
+        setSelection(value)
+        onSelect(value); // Вызываем переданную функцию
+    };
 
-  const numbers = Array.from({ length: end - start + 1 }, (_, i) => start + i);
+    const numbers = Array.from({ length: end - start + 1 }, (_, i) => start + i);
 
-  return (
-    <div className="select-container">
-      {numbers.map((num) => (
-        <button
-          key={num}
-          className="select-button"
-          onClick={() => handleClick(num)}
-        >
-          {num}
-        </button>
-      ))}
-    </div>
-  );
+    return (
+        <div className="select-container">
+            {numbers.map((num) => (
+                <button
+                    key={num}
+                    className={"select-button " + (num === selection ? "selection" : "")}
+                    onClick={() => handleClick(num)}
+                >
+                    {num}
+                </button>
+            ))}
+        </div>
+    );
 };
 
 export default Select;
