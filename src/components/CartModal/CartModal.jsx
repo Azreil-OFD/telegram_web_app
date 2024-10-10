7e9c693cebdb79bb20174c4e65acc39f7e5c671c
Azreil-OFD <fantom2413@gmail.com>
2024-10-10 06:07:30 +0500

    fix

---
+++ src/components/CartModal/CartModal.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './CartModal.css'; // Подключаем CSS стили
+
+const CartModal = ({ isOpen, onClose, onConfirm }) => {
+    if (!isOpen) return null;
+
+    return (
+        <div className="modal-overlay">
+            <div className="modal-container">
+                <p>Вы уверены, что хотите оформить заявку?</p>
+                <div className="modal-buttons">
+                    <button className="cancel-button" onClick={onClose}>Отмена</button>
+                    <button className="confirm-button" onClick={onConfirm}>Подтвердить</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CartModal;
