9f7b232427c848b774b99b31b6aee24a6d115bf1
Azreil-OFD <fantom2413@gmail.com>
2024-10-10 05:58:48 +0500

    fix

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,107 +1,99 @@
 import React, { useEffect, useState, useMemo } from "react";
-import { useParams } from "react-router-dom";
-import Slider from "react-slick"; // Импортируем Slider из react-slick
+import { useParams, useNavigate } from "react-router-dom";
+import Slider from "react-slick";
 import "./ProductItem.css";
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css"; // Стили для слайдера
-import { useTelegram } from "./../../hooks/useTelegram";
-import Select from "./../Select/Select";
+import "slick-carousel/slick/slick-theme.css";
+import { useTelegram } from "../../hooks/useTelegram";
+import Select from "../Select/Select";
 import { useLocalStorage } from "@uidotdev/usehooks";
-import Button from "./../Button/Button";
-import { useNavigate } from "react-router-dom";
+import Button from "../Button/Button";
 
 const ProductItem = () => {
   const [cart, setCart] = useLocalStorage("cart", []);
-  const { categoryID, productID } = useParams(); // Получаем categoryID и productID из URL
+  const { categoryID, productID } = useParams();
   const [product, setProduct] = useState(null);
   const [weight, setWeight] = useState(null);
   const { tg } = useTelegram();
   const [success, setSuccess] = useState(false);
-  const [loading , setLoading]= useState(true)
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+
   const totalPrice = useMemo(() => {
     return product ? (product.attributes.solar / 50) * weight : 0;
   }, [product, weight]);
 
   const handleAddToCart = () => {
-    addToCart({id: product.id,  ...product.attributes, weight });
+    const newProduct = { id: product.id, ...product.attributes, weight };
+    addToCart(newProduct);
+    resetForm();
+    navigate("/");
+  };
+
+  const resetForm = () => {
     setWeight(0);
     setSuccess(true);
-    tg.MainButton.hide()
-    navigate("/")
+    tg.MainButton.hide();
   };
 
-  // Функция для добавления продукта в корзину
-  const addToCart = (product) => {
-    // Проверяем, есть ли продукт уже в корзине
-    const existingProduct = cart.find(
-      (item) => item.id === product.id && item.weight === product.weight
+  const addToCart = (newProduct) => {
+    const updatedCart = cart.map(item =>
+        item.id === newProduct.id && item.weight === newProduct.weight
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
     );
-    if (existingProduct) {
-      // Если уже в корзине, увеличиваем количество
-      const updatedCart = cart.map((item) => {
-        if (item.id === product.id && item.weight === product.weight) {
-          return { ...item, quantity: item.quantity + 1 };
-        }
-        return item;
-      });
-      setCart(updatedCart);
-    } else {
-      // Если нет, добавляем с количеством 1
-      setCart([...cart, { ...product, quantity: 1 }]);
-    }
+    const isProductInCart = updatedCart.some(
+        item => item.id === newProduct.id && item.weight === newProduct.weight
+    );
+
+    setCart(isProductInCart ? updatedCart : [...cart, { ...newProduct, quantity: 1 }]);
   };
 
   useEffect(() => {
-    setLoading(true)
-    // Загружаем данные категорий с продуктами
-    fetch(
-      `https://azreil-ofj-backend-tg-c56e.twc1.net/v1/categories?populate[products][populate]=images`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const category = data.data.find(
-          (category) => category.id === parseInt(categoryID)
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+            `https://azreil-ofj-backend-tg-c56e.twc1.net/v1/categories?populate[products][populate]=images`
         );
+        const data = await response.json();
+        const category = data.data.find(category => category.id === parseInt(categoryID));
         if (category) {
-          const products = category.attributes.products.data;
-          const foundProduct = products.find(
-            (product) => product.id === parseInt(productID)
+          const product = category.attributes.products.data.find(
+              product => product.id === parseInt(productID)
           );
-          if (foundProduct) {
-            setProduct(foundProduct);  
+          setProduct(product || null);
         }
-        
-        }
-      })
-      .catch((error) => console.error("Error fetching product:", error)).finally(() => setLoading(false));
-      tg.MainButton.setText('Оформить заявку!');
-      tg.MainButton.onClick(() => {
-        (async () => {
-          await handleAddToCart()
-        })()
-      });
+      } catch (error) {
+        console.error("Error fetching product:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+    tg.MainButton.setText('Оформить заявку!');
+    tg.MainButton.onClick(() => {
+      (async () => {
+        await handleAddToCart()
+      })()
+    });
 
   }, [categoryID, productID]);
-  if (loading) {
-    return (<h1 className="page-title">Загрузка...</h1>)
-  }
-  if (!product) {
-    return <p>Продукт не найден</p>;
-  }
 
-  const onSelect = (_weight) => {
-    setSuccess(false);
-    setWeight(_weight);
-  };
   useEffect(() => {
-    if(totalPrice !== 0) {
-      tg.MainButton.show()
-    } else [
-      tg.MainButton.hide()
-    ]
-  } , [totalPrice])
-  // Настройки для слайдера
+    if (totalPrice > 0) {
+      tg.MainButton.show();
+    } else {
+      tg.MainButton.hide();
+    }
+  }, [totalPrice, tg]);
+
+  const onSelect = selectedWeight => {
+    setSuccess(false);
+    setWeight(selectedWeight);
+  };
+
   const sliderSettings = {
     dots: true,
     infinite: true,
@@ -110,55 +102,39 @@ const ProductItem = () => {
     slidesToScroll: 1,
   };
 
-  const BASE_URL = "https://azreil-ofj-backend-tg-c56e.twc1.net";
-
-  return (
-    <>
-      <div className="product-item">
-        {product.attributes.images.data.length > 1 && (
-          <Slider {...sliderSettings}>
-            {product.attributes.images.data.map((image, index) => (
-              <div key={index}>
-                <img
-                  src={image.attributes.url}
-                  alt={product.attributes.title}
-                  className="product-image"
-                />
-              </div>
-            ))}
-          </Slider>
-        )}
-        {product.attributes.images.data.length === 1 &&
-          product.attributes.images.data.map((image, index) => (
-            <div key={index}>
-              <img
+  const renderImages = () =>
+      product.attributes.images.data.map((image, index) => (
+          <div key={index}>
+            <img
                 src={image.attributes.url}
                 alt={product.attributes.title}
                 className="product-image"
-              />
-            </div>
-          ))}
+            />
+          </div>
+      ));
 
-        <p>
-          <b>Наименование: </b> {product.attributes.title}
-        </p>
-        <p>
-          <b>Описание: </b> {product.attributes.description}
-        </p>
-        <p>
-          <b>Цена: </b> {product.attributes.solar}₽ / 50 гр
-        </p>
+  if (loading) return <h1 className="page-title">Загрузка...</h1>;
+  if (!product) return <p>Продукт не найден</p>;
+
+  return (
+      <div className="product-item">
+        {product.attributes.images.data.length > 1 ? (
+            <Slider {...sliderSettings}>{renderImages()}</Slider>
+        ) : (
+            renderImages()
+        )}
+
+        <p><b>Наименование: </b>{product.attributes.title}</p>
+        <p><b>Описание: </b>{product.attributes.description}</p>
+        <p><b>Цена: </b>{product.attributes.solar}₽ / 50 гр</p>
 
         <div className="total">Итоговая стоимость: {totalPrice}₽</div>
-        <br />
-        <br />
-        <p>
-          <b>Выберите граммовку</b>
-        </p>
-        <Select start={50} end={1000} step={50} onSelect={onSelect}></Select>
+
+        <p><b>Выберите граммовку</b></p>
+        <Select start={50} end={1000} step={50} onSelect={onSelect} />
+
+
       </div>
-      
-    </>
   );
 };
 
